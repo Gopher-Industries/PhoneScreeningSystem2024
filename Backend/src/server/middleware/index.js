880aed20bc64a1607b37ea9550747a60e1bc8221
Shujian.Lin <2669839973@qq.com>
2024-08-08 20:05:14 +1000

    Remove other content

--- Backend/src/server/middleware/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const applyMiddleware = (app) => {
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-};
-
-module.exports = { applyMiddleware };
