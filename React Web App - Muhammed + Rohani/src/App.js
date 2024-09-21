fbfd008d6365e8666149a3587ad175492e9023b6
vhh064 <131987039+vhh064@users.noreply.github.com>
2024-09-21 21:54:13 +1000

    m sMerge branch 'main' of https://github.com/Gopher-Industries/PhoneScreeningSystem2024

--- React Web App - Muhammed + Rohani/src/App.js
+++ React Web App - Muhammed + Rohani/src/App.js
@@ -4,24 +4,29 @@ import "./App.css";
 import Home from "./Components/home";
 import Login from "./Components/LoginSignin/LoginSignin";
 import Signup from "./Components/Signup/Signup";
+import SetupMfa from "./Components/SetupMfa/SetupMfa";
 import Navbar from "./Components/LandingPage/Navbar";
 import Dashboard from "./Components/Dashboard/Dashboard";
 import TicketDetail from "./Components/TicketDetail/TicketDetail";
 import Calendar from "./Components/Calendar/Calendar";
 
+
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [email, setEmail] = useState("");
+
   return (
-    <div class="main">
+    <div className="main">
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
           <Route exact path="/login" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
           <Route exact path="/Signup" Component={Signup} />
+          <Route exact path="/setup-mfa" Component={SetupMfa} />
           <Route path="/dashboard" Component={Dashboard} />
           <Route path="/TicketDetail" Component={TicketDetail} />
           <Route path="/Calendar" Component={Calendar} />
+          
         </Routes>
       </BrowserRouter>
     </div>
